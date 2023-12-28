effb56e7f74cf31d1e78f481eabfb23bba208d9a
Iker Trueba <truebaescobaliker@gmail.com>
2023-12-28 07:42:31 +0100

    Tic Tac Toe - Smart Computer

---
+++ Madlibs/main.py
@@ -0,0 +1,5 @@
+nombre = input("Escribe tu nombre: ")
+edad = input("Escribe tu edad: ")
+profesion = input("Escribe tu profesión: ")
+madlib = f"Hola a todos, me llamo {nombre} tengo {edad} y trabajo como {profesion}"
+print (madlib)
