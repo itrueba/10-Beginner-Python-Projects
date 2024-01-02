3dbe63b3f2e2178d95bbe2115fbad82e7c1015e3
Iker Trueba <truebaescobaliker@gmail.com>
2024-01-02 17:47:00 +0100

    minesweeper Finished

--- minesweeper/main.py
+++ minesweeper/main.py
@@ -1,4 +1,5 @@
 import random
+import re
 # Creamos un objeto para que represente un tablero
 
 class Board:
@@ -9,7 +10,8 @@ class Board:
         self.num_bombs = num_boms
 
         # Creamos el tablero
-        self.board = self.make_new_board()        
+        self.board = self.make_new_board()
+        self.assing_values_to_board()
 
         # Conjunto para registrar todas las casillas ya excavadas
         self.dug = set() # Si excavamos en la 0,0 - self.dug = {(0,0)}
@@ -31,10 +33,99 @@ class Board:
             bombs_planted +=1
 
         return board
+    
+    def assing_values_to_board(self):
+        for r in range(self.dim_size):
+            for c in range(self.dim_size):
+                if self.board[r][c] == "*":
+                    continue
+                self.board[r][c] = self.get_num_neighboring_bombs(r, c)
+    
+    def get_num_neighboring_bombs(self, row, col):
+        num_neighboring_bombs = 0
+
+        for r in range(max(0, row - 1), min(self.dim_size -1, row + 1)+1):
+            for c in range(max(0, col - 1), min(self.dim_size -1, col + 1)+1):
+                if row == r and col == c:
+                    continue
+                if self.board[r][c] == "*":
+                    num_neighboring_bombs += 1
+
+        return num_neighboring_bombs
+
+    def dig(self, row, col):
+
+        self.dug.add((row, col))
+
+        if self.board[row][col] == "*":
+            return False
+        elif self.board[row][col] > 0:
+            return True
+        
+        for r in range(max(0, row - 1), min(self.dim_size -1, row + 1)+1):
+            for c in range(max(0, col - 1), min(self.dim_size -1, col + 1)+1):
+                if (r, c) in self.dug:
+                    continue
+                self.dig(r, c)
+
+        return True
+
+    def __str__(self):
+        # this is a magic function where if you call print on this object,
+        # it'll print out what this function returns!
+        # return a string that shows the board to the player
+
+        # first let's create a new array that represents what the user would see
+        visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        for row in range(self.dim_size):
+            for col in range(self.dim_size):
+                if (row,col) in self.dug:
+                    visible_board[row][col] = str(self.board[row][col])
+                else:
+                    visible_board[row][col] = ' '
+        
+        # put this together in a string
+        string_rep = ''
+        # get max column widths for printing
+        widths = []
+        for idx in range(self.dim_size):
+            columns = map(lambda x: x[idx], visible_board)
+            widths.append(
+                len(
+                    max(columns, key = len)
+                )
+            )
+
+        # print the csv strings
+        indices = [i for i in range(self.dim_size)]
+        indices_row = '   '
+        cells = []
+        for idx, col in enumerate(indices):
+            format = '%-' + str(widths[idx]) + "s"
+            cells.append(format % (col))
+        indices_row += '  '.join(cells)
+        indices_row += '  \n'
+        
+        for i in range(len(visible_board)):
+            row = visible_board[i]
+            string_rep += f'{i} |'
+            cells = []
+            for idx, col in enumerate(row):
+                format = '%-' + str(widths[idx]) + "s"
+                cells.append(format % (col))
+            string_rep += ' |'.join(cells)
+            string_rep += ' |\n'
+
+        str_len = int(len(string_rep) / self.dim_size)
+        string_rep = indices_row + '-'*str_len + '\n' + string_rep + '-'*str_len
+
+        return string_rep
 
 # Jugar al juego
 def play(dim_size = 10, num_bombs = 10):
     # Paso 1: Crear el tablero y colocar las bombas
+
+    board = Board(dim_size, num_bombs)
 
     # Paso 2: Enseñar el tablero y preguntar al jugador que elija casilla 
 
@@ -44,5 +135,31 @@ def play(dim_size = 10, num_bombs = 10):
 
     # Paso 4: Repetir los pasos 2 - 3a - 3b hasta que no queden bombas. El jugador ha ganado
 
-    
-    pass
+    while len(board.dug) < board.dim_size ** 2 - num_bombs:
+        print(board)
+
+        try:
+            user_input = re.split(',(\\s)*', input("Where would you like to dig? Input as row,col: "))
+            row, col = int(user_input[0]), int(user_input[-1])
+        except ValueError:
+            print("Invalid input. Please enter valid integers.")
+            continue
+        
+        if row < 0 or row >= board.dim_size or col < 0 or col >= dim_size:
+            print("Invalid location. Try again.")
+            continue
+
+        safe = board.dig(row, col)
+        if not safe:
+            break 
+        
+    if safe:
+        print("CONGRATULATIONS!!!! YOU ARE VICTORIOUS!")
+    else:
+        print("SORRY GAME OVER :(")
+        board.dug = [(r,c) for r in range(board.dim_size) for c in range(board.dim_size)]
+        print(board)
+
+
+if __name__ == '__main__':
+    play()
